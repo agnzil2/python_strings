6832a0865fc637c7966029763ef71cad16e82fa5
username <MY_NAME@example.com>
2026-02-17 14:54:29 +0200

    completed three tasks

--- main.py
+++ main.py
@@ -3,5 +3,20 @@ surname = "Petraitis"
 
 if len(name) > len(surname):
     print(name)
+if len(name) == len(surname):
+    print(name, surname)
 else:
     print(surname)
+
+name = "Bruce"
+surname = "Willis"
+
+print(name.upper())
+print(surname.lower())
+
+name = "Bruce"
+surname = "Willis"
+initials = name[0] + surname[0]
+
+print(initials)
+
